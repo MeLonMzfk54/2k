94062173d6fb1607cbcf646b3ed2260432041cb9
MeLonMzfk54 <garifullin.antoshka@mail.ru>
2020-05-18 23:10:35 +0700

    Допилил шапку, починил слайдер и адаптацию
    Допилил шапку, починил слайдер и адаптацию, изменил контейнер в каталоге

--- tatatomato/js/script.js
+++ tatatomato/js/script.js
@@ -13,4 +13,13 @@ document.querySelector(".burger").onclick = function(){
       document.querySelector(".header__block").classList.toggle("visible");
       document.querySelector(".header__bottom").classList.toggle("visible__bottom");
       document.querySelector(".burger").classList.toggle("close");
+        if(document.body.clientWidth <= 404){
+         if(document.querySelector(".burger").classList.contains("close")){
+            document.querySelector(".logo").classList.add("logo__hidden");
+        }else{
+            document.querySelector(".logo").classList.remove("logo__hidden");
+        }   
+        }
   }
+
+
